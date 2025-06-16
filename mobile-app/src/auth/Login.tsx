e2573af937d11b49ea761d1ae88c93b50697f3ab
Dima Snisarenko <snisarenkodima@gmail.com>
2025-06-16 14:41:02 +0200

    feat(auth): hint new users that it is not free

--- mobile-app/src/auth/Login.tsx
+++ mobile-app/src/auth/Login.tsx
@@ -1,7 +1,8 @@
 import { signInWithRedirect } from 'aws-amplify/auth';
 import React, { FC, ReactNode, useContext } from 'react';
-import { Linking, Platform, View } from 'react-native';
+import { Linking, Platform, ScrollView } from 'react-native';
 import { Button, Text, useTheme } from 'react-native-paper';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Loader } from '../loaders/Loader';
 import { AuthContext } from './AuthContext';
 
@@ -21,6 +22,7 @@ export const Login: FC<{
 }> = ({ children }) => {
   const authStatus = useContext(AuthContext);
   const theme = useTheme();
+  const insets = useSafeAreaInsets();
 
   if (authStatus.status === 'undefined') {
     return <Loader>Authenticating...</Loader>;
@@ -31,20 +33,25 @@ export const Login: FC<{
   }
 
   return (
-    <View
-      style={{
+    <ScrollView
+      contentContainerStyle={{
         flexGrow: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        paddingLeft: insets.left + 16,
+        paddingRight: insets.right + 16,
+        gap: 18,
       }}
     >
-      <Text variant="bodyLarge" style={{ marginBottom: 15 }}>
-        Sign in to try the Vocably experience.
+      <Text variant="headlineSmall" style={{ textAlign: 'center' }}>
+        Vocably - a language-learning ecosystem available in desktop browsers
+        and mobile devices.
       </Text>
-      <Button mode="contained" onPress={signIn} style={{ marginBottom: 20 }}>
+      <Text variant="bodyLarge">Sign in to try the Vocably for free.</Text>
+      <Button mode="contained" onPress={signIn}>
         Sign in or Create an account
       </Button>
-      <Text style={{ marginHorizontal: 20, textAlign: 'center' }}>
+      <Text style={{ textAlign: 'center' }}>
         By signing in, you agree to our{' '}
         <Text
           style={{ color: theme.colors.primary }}
@@ -70,12 +77,11 @@ export const Login: FC<{
           onPress={() => signInWithAnIdioticCognitoFlow()}
           style={{
             color: theme.colors.primary,
-            marginTop: 12,
           }}
         >
           I want to sign in with another Google Account.
         </Text>
       )}
-    </View>
+    </ScrollView>
   );
 };
