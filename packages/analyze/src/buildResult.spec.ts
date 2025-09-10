3ecb8d7fac676436ece62eba4232c911a520577b
Dima Snisarenko <snisarenkodima@gmail.com>
2025-09-10 16:09:00 +0200

    chore: fix test

--- packages/analyze/src/buildResult.spec.ts
+++ packages/analyze/src/buildResult.spec.ts
@@ -212,7 +212,7 @@ describe('integration check for translate lambda', () => {
 
     expect(result.value.items.length).toEqual(2);
     expect(result.value.items[0].translation).toHaveSomeOf(
-      'трюк, прием, уловка, фокус, хитрость'
+      'трюк, прием, приём, уловка, фокус, хитрость'
     );
     expect(result.value.items[1].translation).toHaveSomeOf(
       'обмануть, провести'
