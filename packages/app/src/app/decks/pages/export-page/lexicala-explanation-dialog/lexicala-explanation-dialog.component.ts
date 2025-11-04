4a1ceb283371cdfdd845175411fa57d6fa6cae1c
Dima Snisarenko <snisarenkodima@gmail.com>
2025-11-04 10:49:33 +0100

    feat(app): export returned

---
+++ packages/app/src/app/decks/pages/export-page/lexicala-explanation-dialog/lexicala-explanation-dialog.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-lexicala-explanation-dialog',
+  templateUrl: './lexicala-explanation-dialog.component.html',
+  styleUrls: ['./lexicala-explanation-dialog.component.scss'],
+})
+export class LexicalaExplanationDialogComponent implements OnInit {
+  constructor(public dialogRef: MatDialogRef<void>) {}
+
+  ngOnInit(): void {}
+}
