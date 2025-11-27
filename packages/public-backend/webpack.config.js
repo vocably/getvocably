fce6b65b7e18b2c11430a43edd95f74ce4510364
Dima Snisarenko <snisarenkodima@gmail.com>
2025-11-27 13:44:17 +0100

    fix(public analyze): google key

--- packages/public-backend/webpack.config.js
+++ packages/public-backend/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const getModules = require('./webpack/getModules.js');
+const CopyPlugin = require('copy-webpack-plugin');
 const { getEnvironmentVariables } = require('@vocably/webpack');
 const { DefinePlugin } = require('webpack');
 
@@ -24,7 +25,12 @@ module.exports = {
   module: {
     rules: [{ test: /\.tsx?$/, loader: 'ts-loader' }],
   },
-  plugins: [new DefinePlugin(getEnvironmentVariables().stringified)],
+  plugins: [
+    new DefinePlugin(getEnvironmentVariables().stringified),
+    new CopyPlugin({
+      patterns: [{ from: 'google-key.json', to: 'google-key.json' }],
+    }),
+  ],
   output: {
     path: path.join(__dirname, 'dist'),
     libraryTarget: 'commonjs',
