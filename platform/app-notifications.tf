be025af0ba3280776dba80776d69cda040252496
Dima Snisarenko <snisarenkodima@gmail.com>
2025-12-01 11:12:07 +0100

    chore(notifications): disable redundant error alerts

--- platform/app-notifications.tf
+++ platform/app-notifications.tf
@@ -134,32 +134,6 @@ resource "aws_cloudwatch_log_group" "notifications_sender" {
   retention_in_days = 14
 }
 
-resource "aws_cloudwatch_log_metric_filter" "notifications_sender_error" {
-  name           = "error"
-  pattern        = "error"
-  log_group_name = aws_cloudwatch_log_group.notifications_sender.name
-
-  metric_transformation {
-    name      = "vocably-${terraform.workspace}-notifications-sender-error"
-    namespace = "vocably-metrics"
-    value     = "1"
-  }
-}
-
-resource "aws_cloudwatch_metric_alarm" "notifications_sender_error" {
-  alarm_name                = "vocably-${terraform.workspace}-notifications-sender-error"
-  comparison_operator       = "GreaterThanThreshold"
-  evaluation_periods        = "1"
-  metric_name               = aws_cloudwatch_log_metric_filter.notifications_sender_error.metric_transformation[0].name
-  namespace                 = aws_cloudwatch_log_metric_filter.notifications_sender_error.metric_transformation[0].namespace
-  period                    = "3600"
-  statistic                 = "Average"
-  threshold                 = "0"
-  alarm_description         = "${terraform.workspace}: something went wrong while sending notifications"
-  alarm_actions             = [aws_sns_topic.alarm.arn]
-  insufficient_data_actions = []
-}
-
 resource "aws_cloudwatch_event_rule" "notifications_sender" {
   name                = "vocably-${terraform.workspace}-notifications-sender"
   schedule_expression = "rate(1 minute)"
