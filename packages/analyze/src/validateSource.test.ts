d77e56b3660be287fb426bcf51357666123f8293
Dima Snisarenko <snisarenkodima@gmail.com>
2025-12-11 11:50:26 +0100

    feat(analyze): prevent phrases and special symbols from caching

--- packages/analyze/src/validateSource.test.ts
+++ packages/analyze/src/validateSource.test.ts
@@ -1,47 +1,47 @@
 import { validateSource } from './validateSource';
 
 describe('validateSource', () => {
-  it('should return true for a valid source', () => {
-    expect(validateSource('hello')).toBe(true);
-    expect(validateSource('world')).toBe(true);
-    expect(validateSource('testtesttest')).toBe(true);
-  });
-
-  it('should return false if the source contains digits', () => {
-    expect(validateSource('hello123')).toBe(false);
-    expect(validateSource('123world')).toBe(false);
-    expect(validateSource('test1test')).toBe(false);
-  });
-
-  it('should return false if the source contains "@" symbol', () => {
-    expect(validateSource('hello@world')).toBe(false);
-    expect(validateSource('@test')).toBe(false);
-    expect(validateSource('test@')).toBe(false);
-  });
-
-  it('should return false if the source is longer than 200 characters', () => {
-    const longSource = 'a'.repeat(201);
-    expect(validateSource(longSource)).toBe(false);
-  });
-
-  it('should return false if the source is an empty string', () => {
-    expect(validateSource('')).toBe(false);
-  });
-
-  it('should return false on more than 10 words', () => {
+  it('should not include phrases', () => {
     expect(
-      validateSource(
-        'hello world hello world hello world hello world hello world hello world'
-      )
+      validateSource({
+        source: 'some long sentence',
+        partOfSpeech: 'noun ',
+      })
+    ).toBe(true);
+
+    expect(
+      validateSource({
+        source: 'slightly long sentence with extra words',
+        partOfSpeech: 'noun ',
+      })
     ).toBe(false);
-  });
 
-  it('should return true on less than 10 words', () => {
-    expect(validateSource('hello world hello')).toBe(true);
-  });
+    expect(
+      validateSource({
+        source: '123 55',
+        partOfSpeech: 'noun ',
+      })
+    ).toBe(false);
 
-  it('should return true for source with maximum allowed length (200 characters) and no invalid characters or spaces', () => {
-    const maxLenSource = 'a'.repeat(200);
-    expect(validateSource(maxLenSource)).toBe(true);
+    expect(
+      validateSource({
+        source: 'something()',
+        partOfSpeech: 'noun ',
+      })
+    ).toBe(false);
+
+    expect(
+      validateSource({
+        source: 'some type',
+        partOfSpeech: 'noun phrase ',
+      })
+    ).toBe(false);
+
+    expect(
+      validateSource({
+        source: 'phrasal verb',
+        partOfSpeech: 'talk over',
+      })
+    ).toBe(true);
   });
 });
--- packages/analyze/src/validateSource.ts
+++ packages/analyze/src/validateSource.ts
@@ -1,9 +1,22 @@
-export const validateSource = (source: string): boolean => {
-  if (/[\d@]/g.test(source)) {
+type Payload = {
+  source: string;
+  partOfSpeech: string;
+};
+
+export const validateSource = ({ source, partOfSpeech }: Payload): boolean => {
+  if (/[\d@()\[\]]/g.test(source)) {
     return false;
   }
 
+  if (partOfSpeech.includes('phrase')) {
+    return false;
+  }
+
+  if (partOfSpeech === 'idiom' || partOfSpeech === 'phrasal verb') {
+    return true;
+  }
+
   return (
-    source.length <= 200 && source.length >= 1 && source.split(' ').length <= 10
+    source.length <= 30 && source.length >= 1 && source.split(' ').length <= 3
   );
 };
