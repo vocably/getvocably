3036e021e83718335cb31a06ec8812cfaa0fa6c8
Dima Snisarenko <snisarenkodima@gmail.com>
2025-12-16 18:56:22 +0100

    chore(dashboard): set study streak

--- dashboard/set-streak.mjs
+++ dashboard/set-streak.mjs
@@ -10,6 +10,15 @@ const execute = promisify(exec);
 
 const userEmail = process.argv[2];
 
+const daysString = process.argv[3];
+const longestStreakString = process.argv[4];
+
+if (daysString === undefined) {
+  throw new Error('Days must be provided');
+}
+
+const days = parseInt(daysString);
+
 const listUsers = `aws cognito-idp list-users --user-pool-id ${process.env.USER_POOL_ID} --filter "email=\\"${userEmail}\\""`;
 
 const listUsersResult = JSON.parse((await execute(listUsers)).stdout);
@@ -27,6 +36,11 @@ const streakAsIs = (await execute(`aws s3 cp ${s3Path} -`)).stdout || '""';
 const streak = JSON.parse(streakAsIs);
 
 streak.lastStudyDay = new Date().toISOString().split('T')[0];
+streak.days = days;
+
+if (longestStreakString) {
+  streak.longestStreak = parseInt(longestStreakString);
+}
 
 console.log('To be saved', JSON.stringify(streak, null, 2));
 
